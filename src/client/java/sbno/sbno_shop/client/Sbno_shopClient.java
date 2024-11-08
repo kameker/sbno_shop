23dffa70e2778f07a98d5c65980c8e6418f93606
kameker <vsrnpra2@gmail.com>
2024-11-08 21:36:24 +0300

    Initial commit

---
+++ src/client/java/sbno/sbno_shop/client/Sbno_shopClient.java
@@ -0,0 +1,10 @@
+package sbno.sbno_shop.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Sbno_shopClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/sbno/sbno_shop/Sbno_shop.java
@@ -0,0 +1,10 @@
+package sbno.sbno_shop;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sbno_shop implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
